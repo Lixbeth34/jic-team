9a0baa3ec7c326eb825937309778d1c69ab736ef
jairH3110 <98977234+jairH3110@users.noreply.github.com>
2022-10-27 14:21:21 -0500

    Regresionlineal (#5)
    * acomponiendo error y subiendo todo
    
    * comentario pa cerrar #2

---
+++ src/app/getRegresion.spec.ts
@@ -0,0 +1,12 @@
+
+import { getRegresion1 } from './getRegresion1'
+//comentario
+
+describe('getRegresion', () => {
+  it('should return the fact of the numbers', () => {
+    const getResultRegresion = getRegresion1([130,650,99,150,128,302,95,945,368,961],[186,699,132,272,291,331,199,1890,788,1601]);
+    expect(getResultRegresion).toEqual([-22.552532752034267, 1.727932426206986 , 0.9545]);
+
+    
+  });
+});
